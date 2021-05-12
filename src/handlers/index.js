fb5b7ad27b11f6bd107b64bb0759c2c03990290b
Eduardo Cuomo <reduardo7@gmail.com>
2021-05-12 20:04:04 -0300

    Agrego middleware de autorización

--- src/handlers/index.js
+++ src/handlers/index.js
@@ -2,10 +2,11 @@ const express = require('express');
 const usersRouting = require('./users');
 const { ValidationError } = require('../validations/validationError');
 const authRouting = require('./auth');
+const authenticateJWT = require('../middlewares/authenticateJWT');
 
 const apiRouting = express.Router();
 
-apiRouting.use('/api', authRouting, usersRouting);
+apiRouting.use('/api', authRouting, authenticateJWT, usersRouting);
 
 apiRouting.use((err, req, res, next) => {
   if (err instanceof ValidationError) {
