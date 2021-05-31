777ae5235e14e34f3a8a2b9c0e6d31665af12b88
Eduardo Cuomo <reduardo7@gmail.com>
2021-05-31 18:57:10 -0300

    cambio 2

--- src/handlers/users/list.js
+++ src/handlers/users/list.js
@@ -1,10 +1,14 @@
 const database = require('../../database');
+const requestHandler = require('../../middlewares/requestHandler');
 
 // GET /api/users
 module.exports = (route) => {
-  route.get('/', async (req, res) => {
-    const filterName = req.query.filterName;
-    const users = await database.list(filterName);
-    res.json(users);
-  });
+  route.get(
+    '/',
+    requestHandler(async (req, res) => {
+      const filterName = req.query.filterName;
+      const users = await database.list(filterName);
+      res.json(users);
+    })
+  );
 };
