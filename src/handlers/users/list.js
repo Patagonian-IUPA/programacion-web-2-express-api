16b0ef748599283095b1afa3606d57e054c3c07a
Juan Martinez <juan.martinez@patagonian.it>
2021-05-31 19:00:21 -0300

    Cambia metodo list

--- src/handlers/users/list.js
+++ src/handlers/users/list.js
@@ -3,8 +3,7 @@ const database = require('../../database');
 // GET /api/users
 module.exports = (route) => {
   route.get('/', async (req, res) => {
-    const filterName = req.query.filterName;
-    const users = await database.list(filterName);
+    const users = await database.list(req.query.filterName);
     res.json(users);
   });
 };
